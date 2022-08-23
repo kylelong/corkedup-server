3e0602a67a0cc12170013437b1a5ebb142b9d058
kyle long <kylel95@vt.edu>
2022-08-23 00:40:36 -0400

    update port variable naming

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
