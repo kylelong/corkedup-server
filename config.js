a3a428f23d59dd5f1cc0b8e7b35d1a4b37d7f88c
kyle long <kylel95@vt.edu>
2022-08-25 00:52:31 -0400

    update config

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     MONGODB: 'mongodb+srv://kylel95:jopfenmz76sosB5W@cluster1.jipot.mongodb.net/?retryWrites=true&w=majority',
-    SECRET_KEY: '6qm96qNLnVhlU0SmXLmv'
+    SECRET_KEY: '6qm96qNLnVhlU0SmXLmv',
+    YELP_API_KEY: '5hObF9L_APDctOtFxXOSGDWt5bfvPdBHWm7JHoI191sQ73RjayKGTtc1lr_uzkzzmeqR8j1I8UIyxTwelsrI8i_meC8u8sbB_4fHBbCs0PPlcFmnwJ2SNuzRnW5cXnYx'
 };
